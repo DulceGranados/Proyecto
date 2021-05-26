e328db1d12eb78f00fc853eac61426accd554cf2
Your Name <you@example.com>
2021-05-26 16:42:09 -0500

    Add files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaComponent } from './pages/acerca/acerca.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { Error4Component } from './pages/error4/error4.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {
+    path:'inicio',
+    component: InicioComponent
+  },
+  {
+    path:'acerca',
+    component: AcercaComponent
+  },
+  {
+    path:'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: 'error',
+
+    component: Error4Component
+  },
+  {
+    path: '**',
+
+    redirectTo: 'error'
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaComponent } from './pages/acerca/acerca.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { Error4Component } from './pages/error4/error4.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercaComponent,
+    ContactoComponent,
+    MenuComponent,
+    Error4Component,
+  
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
